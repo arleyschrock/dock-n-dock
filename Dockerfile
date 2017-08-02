7ca092e8bab0ce3847df0a873facc42c337c982d
arley schrock <arleyschrock@outlook.com>
2017-08-02 13:16:42 -0500

    .

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM ubuntu:16.10
+# Runs docker on docker. Mostly included here because I needed some customizability of the daemon.json files when doing builds in a hosted build agent running on VSTS
+COPY docker-install.sh /docker-install.sh
+COPY daemon.json /etc/docker/daemon.json
+
+RUN /docker-install.sh
+
+CMD bash
---
+++ docker-install.sh
@@ -0,0 +1,20 @@
+# docker
+sudo apt update
+
+# prerequisites to docker repository registration with apt
+sudo apt install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# docker repository
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt update
+sudo apt install -y docker-ce
